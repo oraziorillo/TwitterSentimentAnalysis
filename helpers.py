New best result, 0.814 achieved with word2vec average, cleaning methods all apart from gensim simple. Need to improve lemmatization, still wrong

helpers.py
@@ -2,6 +2,7 @@ from nltk import ngrams
 import pandas as pd
 import numpy as np
 from tensorflow.keras.utils import to_categorical
+from tqdm import tqdm
 
 def create_labelled_file(name_file, train):
     """
@@ -69,7 +70,7 @@ def create_sentence_vectors(X, Y, word_vector_size, w2v_model):
     counter_of_zero_sentences = 0
     sentence_x = []
     sentence_y = []
-    for sent, label in zip(X, Y):
+    for sent, label in tqdm(zip(X, Y)):
         sentence_vector = np.zeros(word_vector_size) # Probably most common word, we should always find it
         words_in_vocabulary = 0
         for word in sent.split():

word2vec_model_constructor.py
@@ -11,6 +11,8 @@ import multiprocessing
 
 from gensim.models import Word2Vec
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
 parser = argparse.ArgumentParser(description='Builds the word vector model')
 
 parser.add_argument('--train_pickle',
@@ -46,19 +48,7 @@ epochs = int(args.epochs)   # The number of epochs the word2vec model will be tr
 window = int(args.window)
 sg = int(args.sg) == 1
 
-
-
-# Create train_x and test_x sets (shuffle and do split)
-train_test_split = 0.8
-permut = np.random.permutation(df.shape[0])
-train_x = df.iloc[permut[: int(df.shape[0]*train_test_split)]]['sentence']
-train_y = df.iloc[permut[: int(df.shape[0]*train_test_split)]]['label']
-test_x = df.iloc[permut[int(df.shape[0]*train_test_split): ]]['sentence']
-test_y = df.iloc[permut[int(df.shape[0]*train_test_split): ]]['label']
-
-# convert -1 labels into 0
-train_y = train_y.where(train_y == 1, 0) 
-test_y = test_y.where(test_y == 1, 0)
+train_x = df['sentence']
 
 sentences = [row.split() for row in train_x]
 len(sentences)