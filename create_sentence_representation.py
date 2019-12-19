cb267f34f20269ee7db9329ce364f73c9ca84ecb
Stefano <stefano.huber@gmail.com>
2019-12-19 17:09:24 +0100

    Improve comments create_sentence_representation.py module

--- create_sentence_representation.py
+++ create_sentence_representation.py
@@ -10,7 +10,7 @@ from helpers import count_unique_words, count_unique_ngrams, \
 
 import sys
 
-import gensim   # Not sure whether it is better to use gensim or tensorflow :/
+import gensim
 import logging
 from gensim.models.phrases import Phrases, Phraser
 
@@ -48,16 +48,21 @@ parser.add_argument('--output_np_y',
 args = parser.parse_args()
 
 
-if re.match(r".+\.bin", args.w2v_model) != None:
+if re.match(r".*GoogleNews-vectors-negative300", args.w2v_model) != None:
+    # This regex is used because, I think due to different formats, Google's 
+    # pre-trained model must be loaded in a different way than what you would do 
+    # normally
     w2v_model = gensim.models.KeyedVectors.load_word2vec_format(args.w2v_model, binary=True)
 else:
+    # It's a normal Word2Vec model
     w2v_model = Word2Vec.load(args.w2v_model)
 
 word_vector_size = w2v_model.wv.vectors.shape[1]
 
 train_df = pd.read_pickle(args.df_cleaned_sentences) 
 
-limit = train_df.shape[0] if not args.limit else int(args.limit)
+# If the argument limit is not provided, just take all sentences
+limit = int(args.limit) if args.limit is not None else train_df.shape[0]
 
 x = train_df.iloc[:limit]['sentence']
 y = train_df.iloc[:limit]['label']
@@ -69,7 +74,7 @@ y = train_df.iloc[:limit]['label']
 # We need to set labels from the -1, 1 representation into the 0,1 representation
 y = y.where(y == 1, 0) 
 
-
+# Create sentence vectors
 sentence_x, sentence_y = create_sentence_vectors(x, y, word_vector_size, w2v_model)
 
 np.save(args.output_np_x, sentence_x)
