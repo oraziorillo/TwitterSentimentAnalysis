153bd237ffff8742c0a983068615415c6098a154
Catinca <catinca_mujdei@yahoo.ca>
2019-12-19 14:42:40 +0100

    the message

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,21 +1,30 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
 
 
-def naive_bayes_fit(training_data, training_labels):
-    #construct a multinomial naive Bayes classifier based fitted on the given training data and the corresponding class labels
+def naive_bayes_model(training_data, training_labels, n):
+    """
+    Create a naive Bayes classifier pipeline with maximum maximum length of n-gram given.
+
+    :param pandas.core.series.Series training_data: The data the naive Bayes classifier should be trained on
+    :param pandas.core.series.Series training_labels: The labels corresponding to the given training data
+    :param int n: The maximum value of n in the sense of n-grams
+    """
     classifier = Pipeline([
-        ('vect', CountVectorizer(ngram_range=(1, 4))), #add arguments to CountVectorizer for more efficiency
-        ('tfidf', TfidfTransformer()),
+        ('vect', CountVectorizer(ngram_range=(1,n))), #add arguments to CountVectorizer for more efficiency
+        ('tfidf', TfidfTransformer(sublinear_tf=False)),
         ('clf', MultinomialNB()),
     ])
-
     classifier.fit(training_data, training_labels)
     return classifier
 
 
 def naive_bayes_predict(classifier, test_data):
-    #predict the labels of the given test data with the given classifier
+    """
+    Predict the labels of the given test data with the given classifier pipeline.
+    
+    :param sklearn.pipeline.Pipeline classifier: The classifier pipeline with which the labels of the given test data should be predicted
+    :param pandas.core.series.Series test_data: The data the given classifier pipeline should predict the labels of
+    """
     return classifier.predict(test_data)
